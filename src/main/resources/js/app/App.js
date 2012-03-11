72e2c921954dfe3d71781d57ca7a33cd83b932c8
Maxim Kalina <bigpuritz@me.com>
2012-03-11 09:54:15 +0100

    added global ajax error handler

--- src/main/resources/js/app/App.js
+++ src/main/resources/js/app/App.js
@@ -1,8 +1,15 @@
 var App = {
 
-    initialize: function() {
+    initialize:function () {
         var self = this;
-        $("#morePins").click(function(event){
+
+        $("#modal").ajaxError(function (e, jqxhr, settings, exception) {
+            $(this).find(".modal-header h3").html("Server communication error!");
+            $(this).find(".modal-body p").html("Error requesting resource: <strong>" + settings.url + "</strong>");
+            $(this).modal();
+        });
+
+        $("#morePins").click(function (event) {
             event.preventDefault();
             self.loadPins();
         });
@@ -10,31 +17,36 @@ var App = {
         self.loadPins();
     },
 
-    loadPins: function() {
-       var self = this;
-       $.getJSON('rest/pin', function(data) {
-           $.each(data, function(key, val) {
-               var pin = $(Templates.pin(val));
-               pin.children(".close").click(function(event){
+    loadPins:function () {
+        var self = this;
+        $("#morePins").button('loading');
+        $.getJSON('rest/pin', function (data) {
+            $.each(data, function (key, val) {
+                var pin = $(Templates.pin(val));
+                pin.children(".close").click(function (event) {
                     event.preventDefault();
                     $(pin).remove();
-               });
-               pin.find("div.comments a").click(function(event){
+                });
+                pin.find("div.comments button").click(function (event) {
                     event.preventDefault();
                     self.loadComments(pin.children("div.comments"));
-               });
-               $("#app").append(pin);
-           });
-       });
+                });
+                $("#app").append(pin);
+            });
+            $("#morePins").button('reset');
+        });
     },
 
 
-    loadComments: function(container){
-        $.getJSON('rest/comment', function(data) {
-           $.each(data, function(key, val) {
-               var comment = $(Templates.comment(val));
-               container.append(comment);
-           });
+    loadComments:function (container) {
+        var btn = container.find("button");
+        btn.button('loading');
+        $.getJSON('rest/comment', function (data) {
+            $.each(data, function (key, val) {
+                var comment = $(Templates.comment(val));
+                container.append(comment);
+            });
+            btn.button('reset');
         });
     }
 
